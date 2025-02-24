60f1605ac731420149015e724a00feede2eb3e54
dtorrero <david.torrero@gmail.com>
2025-02-24 13:01:20 +0000

    Test bonus

--- git-and-github-practice/script.js
+++ git-and-github-practice/script.js
@@ -3,4 +3,4 @@ soy una hater y lo borro
 // Hola Laura supongo que te apasiona github
 // Hola David gracias por tus modificaciones. EDIT: ok 
 // Editar lo editado again en una nueva rama
-
+*** Empezamos a usar sincronizacion al fork test123***
